bc2cd876863fc52525c36f2cca925006a579362c
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-10-25 05:52:59 +0600

    all filter add and api send complete

--- index.js
+++ index.js
@@ -5,21 +5,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 
-const types = require('./data/types.json')
+
+const category = require('./data/category.json')
 const courses = require("./data/courses.json")
 
 app.get('/', (req, res) => {
-    res.send("programmers-home-server, starts:-4 successfuly");
+    res.send("programmers-home-server,only for server, starts with successfuly");
 });
-
-app.get('/types', (req, res) => {
-    res.send(types);
-})
 
 app.get('/courses', (req, res) => {
     res.send(courses);
-})
+});
+
+app.get('/category', (req, res) => {
+    res.send(category);
+});
+
+
+app.get('/course/:category', (req, res) => {
+    const category = req.params.category;
+    const categoryCourse = courses.filter(course => course.category === category);
+    res.send(categoryCourse);
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const idCourse = courses.filter(course => course.id === id);
+    res.send(idCourse);
+});
 
 app.listen(port, () => {
     console.log(`programmers-home-server running ok ${port}`);
-})
+});
